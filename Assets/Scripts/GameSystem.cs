9dc0967bafad01c9b76336f5503c4420a48a2d1c
Soshi H <soshi@lolol.ooo>
2022-09-04 16:22:11 +0900

    add title and game Scenes. add ParamsSO.

---
+++ Assets/Scripts/GameSystem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        // Use static values
+        Debug.Log(ParamsSO.Entity.TimeLimit);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
---
+++ Assets/Scripts/ParamsSO.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class ParamsSO : ScriptableObject
+{
+    [Header("Time limit")]
+    [Range(30, 60)]
+    public int TimeLimit;
+
+    //Singleton
+
+    public const string PATH = "ParamsSO";
+    private static ParamsSO _entity;
+
+    public static ParamsSO Entity
+    {
+        get
+        {
+            if (_entity == null)
+            {
+                _entity = Resources.Load<ParamsSO>(PATH);
+
+                if (_entity == null)
+                {
+                    Debug.LogError(PATH + " not found");
+                }
+            }
+            return _entity;
+        }
+    }
+}
